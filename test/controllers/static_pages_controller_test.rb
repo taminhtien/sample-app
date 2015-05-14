4425ba030c65ebab735c9e9dcf926ea1cfb7cd65
Tien Ta <taminhtien1993@gmail.com>
2015-05-14 14:43:55 +0000

    Use rake to test this title satisfied test suite

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,7 +9,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
