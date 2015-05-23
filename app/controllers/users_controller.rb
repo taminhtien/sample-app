42bb46c56ca2563d3a92ac9a8d4f647c48e03186
taminhtien <taminhtien1993@gmail.com>
2015-05-23 22:18:23 +0700

    Use mass assignment to pass to create function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,22 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
   def show
     @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Handle a suscessful save
+    else
+      render 'new'
+    end
+  end
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    
   end
 end
