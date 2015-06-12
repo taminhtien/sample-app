c440c1152e583faeb91e4e8d6e6430ed6ab6590c
Ta Minh Tien <taminhtien1993@gmail.com>
2015-06-12 23:13:59 +0700

    Add logged_in method

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,4 +10,9 @@ module SessionsHelper
   	def current_user
     	@current_user ||= User.find_by(id: session[:user_id])
   	end
+
+  	# Returns true if the user is logged in and false otherwise
+  	def logged_in?
+  		!current_user.nil?
+  	end
 end
