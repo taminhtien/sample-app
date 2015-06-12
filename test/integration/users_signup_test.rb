51f7c680f996cff4d67a84a3cc9a8dd744348d54
Ta Minh Tien <taminhtien1993@gmail.com>
2015-06-12 12:54:57 +0700

    Add some tests to make test detailed

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,15 +10,17 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                 password_confirmation:  "bar" }
     end
     assert_template 'users/new'
+    assert_select 'div#<CSS id for error explanation>'
+    assert_select 'div.<CSS class for field with error>'
   end
 
   test "valid sign up information" do
     get signup_path
     assert_difference 'User.count', 1 do
-      post_via_redirect users_path, user: { name:                   "Tien Ta",
-                                            email:                  "tienta@gmail.com",
-                                            password:               "tienta",
-                                            password_confirmation:  "tienta" }
+      post_via_redirect users_path, user: { name:                   "Tien Ta1",
+                                            email:                  "tienta1@gmail.com",
+                                            password:               "tienta1",
+                                            password_confirmation:  "tienta1" }
     end
     assert_template 'users/show'
   end
