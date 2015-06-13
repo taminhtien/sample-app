f7f35e0bab121e29480958fb92c3264b0edf03a4
Ta Minh Tien <taminhtien1993@gmail.com>
2015-06-13 21:14:43 +0700

    Add forget method to forget remember user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,37 +7,45 @@ module SessionsHelper
 	end
 
 	# Returns the current logged-in user (if any).
-  	def current_user
-  		# Assigns user_id = session[:user_id]
-  		if (user_id = session[:user_id]) # If session[:user_id] exists
-  			@current_user ||= User.find_by(id: user_id)
-  		elsif (user_id = cookies.signed[:user_id]) # If user exists in cookies
-  			user = User.find_by(id: user_id) # Find user_id exists in database
-  			if user && user.authenticated?(cookies[:remember_token])
-  				log_in user
-  				@current_user = user
-  			end
-  		end
-  	end
+	def current_user
+		# Assigns user_id = session[:user_id]
+		if (user_id = session[:user_id]) # If session[:user_id] exists
+			@current_user ||= User.find_by(id: user_id)
+		elsif (user_id = cookies.signed[:user_id]) # If user exists in cookies
+			user = User.find_by(id: user_id) # Find user_id exists in database
+			if user && user.authenticated?(cookies[:remember_token])
+				log_in user
+				@current_user = user
+			end
+		end
+	end
 
-  	# Returns true if the user is logged in and false otherwise
-  	# The purpose of this method is help view to render page properly
-  	def logged_in?
-  		!current_user.nil?
-  	end
+	# Returns true if the user is logged in and false otherwise
+	# The purpose of this method is help view to render page properly
+	def logged_in?
+		!current_user.nil?
+	end
 
-  	# Logs out the current user
-  	def log_out
-  		session.delete(:user_id)
-  		@current_user = nil
-  	end
+	# Logs out the current user
+	def log_out
+    forget(current_user)
+		session.delete(:user_id)
+		@current_user = nil
+	end
 
-  	# Remembers a user in a persistent session
-  	def remember(user)
-  		user.remember # Creates the token and save the digest into database
-  		 # permanent method will persist data for 20 years
-  		 # signed method will encrypt data because only cookies[] doesn't do that
-  		cookies.permanent.signed[:user_id] = user.id
-  		cookies.permanent[:remember_token] = user.remember_token # remember_token attribute
-  	end
+	# Remembers a user in a persistent session
+	def remember(user)
+		user.remember # Creates the token and save the digest into database
+		 # permanent method will persist data for 20 years
+		 # signed method will encrypt data because only cookies[] doesn't do that
+		cookies.permanent.signed[:user_id] = user.id
+		cookies.permanent[:remember_token] = user.remember_token # remember_token attribute
+	end
+
+  # Forgets a persistent session
+  def forget(user)
+    user.forget # Update :remember_digest to
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
 end
