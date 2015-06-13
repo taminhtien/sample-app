1a786e4f0ad95e955d42894582f5ad01e65a3f9c
Ta Minh Tien <taminhtien1993@gmail.com>
2015-06-13 22:55:38 +0700

    Add check box remember to logging in page

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -53,7 +53,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     # Simulate a user click log out in a second windows
     delete logout_path
     follow_redirect!
-    assert_select "a[href=?]", login_path,
+    assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,       count: 0
     assert_select "a[href=?]", user_path(@user),  count: 0
   end
