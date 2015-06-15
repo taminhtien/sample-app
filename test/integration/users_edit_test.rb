65bc7af8fc1385f1dc44d955828999c635765496
Ta Minh Tien <taminhtien1993@gmail.com>
2015-06-15 23:44:25 +0700

    Fix test user edit to pass

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -6,6 +6,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "unsuccessful edit" do 
+  	log_in_as(@user)
   	get edit_user_path(@user)
   	assert_template 'users/edit'
   	patch user_path(@user), user: { name: "",
@@ -15,7 +16,8 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   	assert_template 'users/edit'
   end
 
-  test "successful edit" do 
+  test "successful edit" do
+  	log_in_as(@user) 
   	get edit_user_path(@user)
   	assert_template 'users/edit'
   	name = "Tien2 Ta"
