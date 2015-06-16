49d6376d935948d13bd76b718132de028c1407d2
Ta Minh Tien <taminhtien1993@gmail.com>
2015-06-16 22:58:47 +0700

    Add destroy action and the filter to check admin user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   # The filter will act on edit and update actions
   before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
 
   def new
     @user = User.new
@@ -64,5 +65,17 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
+    end
+
+    # Detroys an user
+    def destroy
+      User.find(params[:id]).destroy
+      flash[:success] = "User deleted"
+      redirect_to users_url
+    end
+
+    # Confirms an admin user
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
