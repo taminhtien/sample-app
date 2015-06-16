bf9cbf97f3694601affd3bf5a6e9f9b06b6c7e68
Ta Minh Tien <taminhtien1993@gmail.com>
2015-06-16 18:29:02 +0700

    Add index action and show all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   # The filter will act on edit and update actions
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
 
   def new
@@ -9,6 +9,10 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.all
   end
 
   # Sign up process
