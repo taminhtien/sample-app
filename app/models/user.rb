d02d91d74a493d97110564b1d515b4136dd92f3d
Ta Minh Tien <taminhtien1993@gmail.com>
2015-06-18 13:27:10 +0700

    Refactor sample database and fix the redundant of colon in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,44 +1,54 @@
 class User < ActiveRecord::Base
-    attr_accessor :remember_token
-    before_save { self.email = email.downcase }
-    validates :name,    presence: true,
-                        length: { maximum: 50 }
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email,   presence: true,
-                        length: { maximum: 255 },
-                        format: { with: VALID_EMAIL_REGEX },
-                        uniqueness: { case_sensitive: false }
-    has_secure_password
-    validates :password, presence: true, length: { minimum: 6}, allow_nil: true
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  validates :name,    presence: true,
+                      length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email,   presence: true,
+                      length: { maximum: 255 },
+                      format: { with: VALID_EMAIL_REGEX },
+                      uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6}, allow_nil: true
 
-    class << self
-        # Returns the hash digest of the given string
-        def digest(string)
-            cost = ActiveModel::SecurePassword.min_cost ?   BCrypt::Engine::MIN_COST :
-                                                            BCrypt::Engine.cost
-            BCrypt::Password.create(string, cost: cost)
-        end
-
-        # Returns a random token
-        def new_token
-            SecureRandom.urlsafe_base64
-        end
+  class << self
+    # Returns the hash digest of the given string
+    def digest(string)
+        cost = ActiveModel::SecurePassword.min_cost ?   BCrypt::Engine::MIN_COST :
+                                                        BCrypt::Engine.cost
+        BCrypt::Password.create(string, cost: cost)
     end
 
-    # Remembers a user in the database for use in persistent sessions
-    def remember
-        self.remember_token = User.new_token
-        update_attribute(:remember_digest, User.digest(remember_token))
+    # Returns a random token
+    def new_token
+        SecureRandom.urlsafe_base64
+    end
+  end
+
+  # Remembers a user in the database for use in persistent sessions
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Forgets a user
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
+  # Returns true if the given token matches the digest
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  private
+    def downcase_email
+      self.email = email.downcase
     end
 
-    # Forgets a user
-    def forget
-        update_attribute(:remember_digest, nil)
-    end
-
-    # Returns true if the given token matches the digest
-    def authenticated?(remember_token)
-        return false if remember_digest.nil?
-        BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    def create_activation_digest
+        self.activation_token = User.new_token # Save as a class User variable
+        self.activation_digest = User.digest(activation_token) # Save into the database
     end
 end
