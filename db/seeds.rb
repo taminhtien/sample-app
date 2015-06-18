b56b96b5edfd7541eb5f03760cff0cf88735cc70
Ta Minh Tien <taminhtien1993@gmail.com>
2015-06-18 13:37:34 +0700

    Fix missing comma

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,8 @@ User.create!(	name:  "Example User",
              	email: "example@railstutorial.org",
              	password:              "foobar",
              	password_confirmation: "foobar",
-             	admin: true
-             	activated: true
+             	admin: true,
+             	activated: true,
              	activated_at: Time.zone.now)
 
 99.times do |n|
@@ -21,6 +21,6 @@ User.create!(	name:  "Example User",
                	email: email,
                	password:              password,
                	password_confirmation: password,
-               	activated: true
+               	activated: true,
                	activated_at: Time.zone.now)
 end
