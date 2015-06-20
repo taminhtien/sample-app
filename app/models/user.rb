e79e9a387f33181843b97e0371582a3f44c80965
Ta Minh Tien <taminhtien1993@gmail.com>
2015-06-21 00:04:45 +0700

    Move some of code from controller into model

--- app/models/user.rb
+++ app/models/user.rb
@@ -44,6 +44,17 @@ class User < ActiveRecord::Base
     BCrypt::Password.new(digest).is_password?(token)
   end
 
+  # Activates an account
+  def activate
+    update_attribute(:activated, true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  # Sends activation email
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
   private
     def downcase_email
       self.email = email.downcase
