531380636a855f5810d5543a2fc12cc87243e84b
Ta Minh Tien <taminhtien1993@gmail.com>
2015-06-25 17:09:35 +0700

    Create fixtures data and make a test for validate the order of microposts

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class MicropostTest < ActiveSupport::TestCase
   def setup
   	@user = users(:michael)
-  	@micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+		@micropost = @user.microposts.build(content: "Lorem ipsum")
   end
 
   test "should be valid" do
@@ -23,5 +23,9 @@ class MicropostTest < ActiveSupport::TestCase
 	test "content should be at most 140 characters" do
 		@micropost.content = 'a' * 141;
 		assert_not @micropost.valid?
+	end
+
+	test "order should be most recent first" do
+		assert_equal microposts(:most_recent), Micropost.first
 	end
 end
