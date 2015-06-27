d823c0136c93b90114ee6e0ebce376ff44e8129f
Ta Minh Tien <taminhtien1993@gmail.com>
2015-06-27 20:14:31 +0700

    Add private for logged_in_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,12 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
 
 	# Confirm a logged-in user
-  def logged_in_user 
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
-  end
+	private
+	  def logged_in_user 
+	    unless logged_in?
+	      store_location
+	      flash[:danger] = "Please log in."
+	      redirect_to login_url
+	    end
+	  end
 end
