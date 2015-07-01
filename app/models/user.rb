d10d02f6afb1416b753e48eae314db3ebd0a00dc
Ta Minh Tien <taminhtien1993@gmail.com>
2015-07-01 16:34:44 +0700

    Add the passive relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
 
-  has_many :active_relationships,  class_name: "Relationship",
-                                  foreign_key: "follower_id",
-                                  dependent: :destroy
+  has_many :active_relationships,   class_name: "Relationship",
+                                    foreign_key: "follower_id",
+                                    dependent: :destroy
+  has_many :passive_relationships,  class_name: "Relationship",
+                                    foreign_key: "followed_id",
+                                    dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships
 
   attr_accessor :remember_token, :activation_token, :reset_token
   
