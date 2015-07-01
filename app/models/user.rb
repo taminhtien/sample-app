a7d0eccc2b48147f3e7f318c2aa1f0a6fcbaa1ad
Ta Minh Tien <taminhtien1993@gmail.com>
2015-07-01 16:17:36 +0700

    Add follow, unfollow, following? methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -84,6 +84,21 @@ class User < ActiveRecord::Base
     Micropost.where("user_id = ?", id)
   end
 
+  # Follows a user
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  # Unfollows a user
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # Returns true if the current user is following the other user
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
   private
 
     def downcase_email
