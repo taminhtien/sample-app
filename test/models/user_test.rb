0841f0b29891df3da7997de5dc743f09b943e882
Ta Minh Tien <taminhtien1993@gmail.com>
2015-07-03 16:30:36 +0700

    Add test for the status feed

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,31 +4,31 @@ class UserTest < ActiveSupport::TestCase
   def setup
     @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
   end
-  
+
   test "should be valid" do
     assert @user.valid?
   end
-  
+
   test "user name should be present" do
     @user.name = '   '
     assert_not @user.valid?
   end
-  
+
   test "email should be present" do
     @user.email = '   '
     assert_not @user.valid?
   end
-  
-  test "name should not too long" do 
+
+  test "name should not too long" do
     @user.name = "a" * 51
     assert_not @user.valid?
   end
-  
-  test "email should not too long" do 
+
+  test "email should not too long" do
     @user.email = "a" * 244 + "@example.com"
     assert_not @user.valid?
   end
-  
+
   test "email validation should accept valid addresses" do
     valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
                          first.last@foo.jp alice+bob@baz.cn]
@@ -37,7 +37,7 @@ class UserTest < ActiveSupport::TestCase
       assert @user.valid?, "#{valid_address.inspect} should be valid"
     end
   end
-  
+
  test "email validation should reject invalid addresses" do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
                            foo@bar_baz.com foo@bar+baz.com]
@@ -46,15 +46,15 @@ class UserTest < ActiveSupport::TestCase
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
   end
-  
+
   test "email addresses should be unique" do
     duplicate_user = @user.dup
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
   end
-  
-  test "password should have a minimum length" do 
+
+  test "password should have a minimum length" do
     @user.password = @user.password_confirmation = "a" * 5;
     assert_not @user.valid?
   end
@@ -81,4 +81,26 @@ class UserTest < ActiveSupport::TestCase
     michael.unfollow(archer)
     assert_not michael.following?(archer)
   end
+
+  test "feed should have a right posts" do
+    michael = users(:michael)
+    archer = users(:archer)
+    lana = users(:lana)
+
+    # Posts from followed user
+    lana.microposts.each do |post_following|
+      assert michael.feed.include?(post_following)
+    end
+
+    # Posts from self
+    michael.microposts.each do |post_self|
+      assert michael.feed.include?(post_self)
+    end
+
+    # Posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not michael.feed.include?(post_unfollowed)
+    end
+  end
+
 end
