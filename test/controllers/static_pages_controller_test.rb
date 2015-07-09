78bf24b375337e012be26c01c80230219b53df7e
Ta Minh Tien <taminhtien1993@gmail.com>
2015-07-10 00:44:03 +0700

    Finish style home page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,13 +3,13 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionController::TestCase
   
   def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
+    @base_title = "Twitter"
   end
   
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Twitter"
   end
 
   test "should get help" do
