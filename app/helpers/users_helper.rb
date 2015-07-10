1f5c7d684194bcc1193216519abf8a106c0311dc
Ta Minh Tien <taminhtien1993@gmail.com>
2015-07-10 17:10:12 +0700

    Finish restyle layout

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,4 +6,10 @@ module UsersHelper
     gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
+
+  def avatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}?s=200"
+    image_tag(gravatar_url, alt: user.name, class: "avatar")
+  end
 end
