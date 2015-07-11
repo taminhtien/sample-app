d5533f2dc29231c4a229c47982945954547d6b4d
Ta Minh Tien <taminhtien1993@gmail.com>
2015-07-11 22:39:24 +0700

    Fix avatar header

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,15 +1,23 @@
 module UsersHelper
   # Return gavatar for given user
   def gravatar_for(user, options)
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    size = options[:size]
-    gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  	if user.avatar?
+    	image_tag(user.avatar.url, class: "gravatar")
+    else
+    	gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    	size = options[:size]
+    	gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    	image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    end
   end
 
   def avatar_for(user)
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}?s=200"
-    image_tag(gravatar_url, alt: user.name, class: "avatar")
+  	if user.avatar?
+    	image_tag(user.avatar.url, class: "avatar", style: "width: 200px; height: 200px;")
+    else
+    	gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+	    gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}?s=200"
+	    image_tag(gravatar_url, alt: user.name, class: "avatar")
+    end
   end
 end
