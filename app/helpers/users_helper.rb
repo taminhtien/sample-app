8b44427138e167920a9cd67974c71686279c7ff8
Ta Minh Tien <taminhtien1993@gmail.com>
2015-07-12 23:40:45 +0700

    Delete gravatar and replace by avatar helper method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,23 +1,10 @@
 module UsersHelper
-  # Return gavatar for given user
-  def gravatar_for(user, options)
+  def avatar(user)
   	if user.avatar?
-    	image_tag(user.avatar.url, class: "gravatar")
+    	user.avatar.url
     else
     	gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    	size = options[:size]
-    	gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    	image_tag(gravatar_url, alt: user.name, class: "gravatar")
-    end
-  end
-
-  def avatar_for(user)
-  	if user.avatar?
-    	image_tag(user.avatar.url, class: "avatar", style: "width: 200px; height: 200px;")
-    else
-    	gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-	    gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}?s=200"
-	    image_tag(gravatar_url, alt: user.name, class: "avatar")
+	    gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}"
     end
   end
 end
