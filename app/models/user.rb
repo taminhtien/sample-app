14c804906c7c2796e05a33166ec1ed8220d65d6e
Ta Minh Tien <taminhtien1993@gmail.com>
2015-07-15 00:33:08 +0700

    Connect devise with omniauthable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :omniauthable, :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable,
 
   has_many :microposts, dependent: :destroy
 
