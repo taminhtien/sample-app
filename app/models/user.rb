329d8110b1a142a557ee8beaafae9bc3c7f619ae
Ta Minh Tien <taminhtien1993@gmail.com>
2015-07-15 00:28:43 +0700

    Remove has_secure_password from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,14 +19,15 @@ class User < ActiveRecord::Base
 
   before_save :downcase_email
   before_create :create_activation_digest
-  validates :name,    presence: true,
+  #validates :name,    presence: true,
+  validates :name,
                       length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email,   presence: true,
                       length: { maximum: 255 },
                       format: { with: VALID_EMAIL_REGEX },
                       uniqueness: { case_sensitive: false }
-  has_secure_password
+  #has_secure_password
   validates :password, presence: true, length: { minimum: 6}, allow_nil: true
 
   mount_uploader :avatar, PictureUploader
