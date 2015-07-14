d2be8a69f9c93f56b74e5198836bfa308871d614
Ta Minh Tien <taminhtien1993@gmail.com>
2015-07-15 00:50:22 +0700

    Generate identity model

---
+++ db/migrate/20150714174959_create_identities.rb
@@ -0,0 +1,20 @@
+class CreateIdentities < ActiveRecord::Migration
+  def change
+    create_table :identities do |t|
+      t.references :user, index: true
+      t.string :provider
+      t.string :accesstoken
+      t.string :refreshtoken
+      t.string :uid
+      t.string :name
+      t.string :email
+      t.string :nickname
+      t.string :image
+      t.string :phone
+      t.string :urls
+
+      t.timestamps null: false
+    end
+    add_foreign_key :identities, :users
+  end
+end
